587a4f285ee158f37046eb145aff6981e4b90a18
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-12-19 19:10:20 +0000

    revert flake changes

--- script/setup.sh
+++ script/setup.sh
@@ -31,8 +31,6 @@ if /bin/test -z "${NUKE_PATH:-}"; then
     add-optional-dep-to-bin git
     add-optional-dep-to-bin swift
     add-optional-dep-to-bin swiftly
-    add-optional-dep-to-bin java
-    add-optional-dep-to-bin python3
 
     export PATH="${PWD}/.deps/bin:/bin:/usr/bin"
     chmod +x .deps/bin/*
